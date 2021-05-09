a15cfd4659e16d69d73faa0a5c9c7401004b3f92
hasan683 <hsourov511@gmail.com>
2021-05-09 12:47:41 +0600

    first commit

---
+++ app/Http/Controllers/EmployeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\employe;
+
+
+class EmployeController extends Controller
+{
+    
+    public function index()
+    {
+        $employes=employe::get('designation');
+        return view('employe', ['employe' => $employes]);
+    } 
+}
---
+++ resources/views/employe.blade.php
@@ -0,0 +1,66 @@
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <style>
+    table {
+      border-collapse: collapse;
+      width: 100%;
+    }
+
+    td,th {
+      border: 1px solid #dddddd;
+      text-align: left;
+      padding: 8px;
+    }
+
+  </style>
+</head>
+
+<body>
+ 
+<table>
+  <tr>
+    <th>id</th>
+    <th>name</th>
+    <th>phone</th>
+    <th>address</th>
+    <th>designation</th>
+    <th>salary</th>
+    <th>joining_date</th>
+  </tr>
+
+  
+
+  <style>
+  .bg{
+    background-color:red;
+    border:1px solid red;
+    color:white;
+  }
+  </style>
+
+@foreach($employe as $employ)
+
+<tr>
+  <td>{{$employ->id}}</td>
+  <td>{{$employ->name}}</td>
+  <td>{{$employ->phone}}</td>
+  <td>{{$employ->address}}</td>
+  <td>{{$employ->designation}}</td>
+  <td>{{$employ->salary}}</td>
+  <td>{{$employ->joining_date}}</td>
+
+</tr> 
+  
+@endforeach
+
+</table>
+
+
+</body>
+
+</html>
+
